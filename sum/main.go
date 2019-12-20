e73a820f87d98ce7598b06f003a9ec7b0a9ef34d
Eric Njogu <kunadawa@gmail.com>
2019-12-20 13:27:28 +0300

    lesson: logging errors to stderr
    credit - https://stackoverflow.com/a/28935032/315385

--- sum/main.go
+++ sum/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
   "strconv"
+  "log"
 )
 
 /*
@@ -13,7 +14,11 @@ func main() {
 	var sum int
 	for _, arg := range os.Args[1:] {
     num, err := strconv.Atoi(arg)
-		if err == nil {sum += num}
+		if err == nil {
+      sum += num
+    } else {
+      log.Printf("num: %v, err: %v", num, err)
+    }
 	}
 	fmt.Println(sum)
 }
