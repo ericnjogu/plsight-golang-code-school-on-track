5f2eb14407759f60ffabf3ec24ffa4d631b2d53d
Eric Njogu <kunadawa@gmail.com>
2019-12-20 13:40:19 +0300

    lesson: using 'append' to grow an array

--- sum/main.go
+++ sum/main.go
@@ -12,13 +12,15 @@ sum up all args
 */
 func main() {
 	var sum int
+  var badArgs []string
 	for _, arg := range os.Args[1:] {
     num, err := strconv.Atoi(arg)
 		if err == nil {
       sum += num
     } else {
-      log.Printf("num: %v, err: %v", num, err)
+      badArgs = append(badArgs, arg)
     }
 	}
+  log.Printf("invalid args: %v", badArgs)
 	fmt.Println(sum)
 }
